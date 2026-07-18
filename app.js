1b51ff2f3b7d5f188e6b5f9eb0dbf5a0d32e9d79
Meno <menolycasch2006@gmail.com>
2026-07-18 09:51:37 +0530

    Commit 3 : Refactor submit button logic and enhance user feedback for number guessing game

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-let num = Math.random();
+function submitBtn() {
 
-let randomNum = Math.floor(num);
+    let randomNum = Math.floor(Math.random() * 10) + 1;
+    console.log(randomNum);
 
-console.log(randomNum);
+    let numinput = Number(document.getElementById("guessInput").value);
+    let heading = document.getElementById("result");
 
+    if (numinput > randomNum) {
+        heading.innerText = "Too High!";
+    }
+    else if (numinput < randomNum) {
+        heading.innerText = "Too Low!";
+    }
+    else {
+        heading.innerText = "You guessed the Number!";
+    }
+}
 
+function resetBtn(){
+    document.getElementById("guessInput").value = "";
+    document.getElementById("result").innerText = "";
+}
